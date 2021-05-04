7c3f8d66dd631d6fa5f03e6e9c08f277f0979b7e
nicholas <nicholasjcole1@gmail.com>
2021-05-03 23:32:08 -0500

    new mdb pull code

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
 from flask import Flask
+import pymongo
+import tweet_scraper.get_app_idea_tweets as gait
+
+MONGO_PW=os.environ.get('MONGO_PW')
 
 app = Flask(__name__)
 
 @app.route("/")
 def home_view():
+    db = gait.connect_mdb()
+    tweets = db.tweet_data.find({}).limit(10)
     return "<h1>Test app</h1>"
