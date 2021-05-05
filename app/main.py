05b287241eef4941b7aea81cdad0afa358b97069
nicholas <nicholasjcole1@gmail.com>
2021-05-04 22:48:28 -0500

    added MDB pull

--- app/main.py
+++ app/main.py
@@ -2,11 +2,13 @@ from flask import Flask
 import pymongo
 import app.tweet_scraper.get_app_idea_tweets as gait
 import os
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def home_view():
+@app.route("/index")
+def index():
     db = gait.connect_mdb()
     tweets = db.tweet_data.find({}).limit(10)
-    return "<h1>Test app</h1>"
+    return render_template('index.html', tweets=tweets)
