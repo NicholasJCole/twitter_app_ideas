2f4bc7548ade23531e6df28b1d7a7a1779566b53
nicholas <nicholasjcole1@gmail.com>
2021-05-12 00:57:02 -0500

    fixed deleted tweet issue

--- app/tweet_scraper/get_replied_to.py
+++ app/tweet_scraper/get_replied_to.py
@@ -18,32 +18,36 @@ def retrieve_select_mdb():
     # tweets = db.tweet_data.find({"$and": [{"in_reply_to_status_id_str": {"$exists": "true"}}, {"in_reply_to_status_id_str": {"$ne": None}}]})
     tweets = db.tweet_data.find({"$and": [{"in_reply_to_status_id_str": {"$exists": "true"}},
                                           {"in_reply_to_status_id_str": {"$ne": None}},
-                                          {"in_reply_to_content": {"$exists": False}}]})
+                                          {"in_reply_to_content": {"$exists": False}},
+                                          {"tweet_deleted": {"$eq": "No"}}]})
     return tweets
 
 def add_replied_tweet(tweets, limit):
     count = 0
     db = gait.connect_mdb()
     min_limit = min(tweets.count(), limit)
-    for tweet in tweets[0:min_limit]:
-       try:
-            in_reply_to_tweet = get_tweet(tweet['in_reply_to_status_id_str'])
-            db.tweet_data.update_one({"tweet_id": tweet['tweet_id']}, {"$set": {"in_reply_to_content": in_reply_to_tweet['full_text']}})
-            count+=1
-            print("Added {} tweets".format(count))
-       except:
-           error_msg = sys.exc_info()
-           if error_msg[1].__dict__['reason'] == "[{'code': 179, 'message': 'Sorry, you are not authorized to see this status.'}]":
-               count += 1
-               db.tweet_data.update_one({"tweet_id": tweet['tweet_id']}, {"$set": {"tweet_deleted": "Private"}})
-               print("Tweet not found error: {}".format(error_msg[1]))
-           elif error_msg[1].__dict__['reason'] == "[{'code': 144, 'message': 'No status found with that ID.'}]":
-               count += 1
-               db.tweet_data.update_one({"tweet_id": tweet['tweet_id']}, {"$set": {"tweet_deleted": "Deleted"}})
-               print("Tweet not found error: {}".format(error_msg[1]))
-           else:
-               print("Other error: {}".format(error_msg[1]))
-    count_text = str(count) + " tweets updated"
+    if min_limit > 0:
+        for tweet in tweets[0:min_limit]:
+           try:
+                in_reply_to_tweet = get_tweet(tweet['in_reply_to_status_id_str'])
+                db.tweet_data.update_one({"tweet_id": tweet['tweet_id']}, {"$set": {"in_reply_to_content": in_reply_to_tweet['full_text']}})
+                count+=1
+                print("Added {} tweets".format(count))
+           except:
+               error_msg = sys.exc_info()
+               if error_msg[1].__dict__['reason'] == "[{'code': 179, 'message': 'Sorry, you are not authorized to see this status.'}]":
+                   count += 1
+                   db.tweet_data.update_one({"tweet_id": tweet['tweet_id']}, {"$set": {"tweet_deleted": "Private"}})
+                   print("Tweet not found error: {}".format(error_msg[1]))
+               elif error_msg[1].__dict__['reason'] == "[{'code': 144, 'message': 'No status found with that ID.'}]":
+                   count += 1
+                   db.tweet_data.update_one({"tweet_id": tweet['tweet_id']}, {"$set": {"tweet_deleted": "Deleted"}})
+                   print("Tweet not found error: {}".format(error_msg[1]))
+               else:
+                   print("Other error: {}".format(error_msg[1]))
+        count_text = str(count) + " tweets updated"
+    else:
+        count_text = "No tweets to update"
     return count_text
 
 def get_tweet(tweet_id):
